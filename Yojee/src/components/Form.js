4ac4d352120aab6c28bf7927e45193fb8c99f1e8
binhan16 <an.dang@s3corp.com.vn>
2022-01-18 11:58:20 +0700

    push form fix

--- Yojee/src/components/Form.js
+++ Yojee/src/components/Form.js
@@ -1,5 +1,8 @@
 import React from "react";
 import "./Form.css";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
 
 const btnPink = {
   outline: "none!important",
@@ -12,6 +15,11 @@ const btnPink = {
   transition: "all .3s",
   marginTop: "10px",
 };
+
+const SignupSchema=yup.object().shape({
+    firstName: yup.string().required(),
+    lastName: yup.string().required(),
+})
 
 function Form() {
   return (
@@ -30,6 +38,60 @@ function Form() {
               </div>
               <div className="form-input col-12 col-sm-6">
                 <form>
+                  <div>
+                    <label>First Name</label>
+                    <input
+                      placeholder="Your name..."
+                      {...register("firstName")}
+                    />
+                  </div>
+                  <div>
+                    <label>Last Name</label>
+                    <input
+                      placeholder="Your last name..."
+                      {...register("lastName")}
+                    />
+                  </div>
+                  <div>
+                    <label>Contact Number</label>
+                    <input
+                      placeholder="Your contact number..."
+                      {...register("contactNumber")}
+                    />
+                  </div>
+                  <div>
+                    <label>Company</label>
+                    <input
+                      placeholder="Your company..."
+                      {...register("company")}
+                    />
+                  </div>
+                  <div>
+                    <label>Email</label>
+                    <input placeholder="Your email..." {...register("email")} />
+                  </div>
+                  <div>
+                    <label>Country</label>
+                    <select {...register("country")}>
+                      <option value="usa">USA</option>
+                      <option value="australia">Australia</option>
+                      <option value="canada">Canada</option>
+                    </select>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default Form;
+
+{
+  /* <form>
                   <div className="form-group">
                     <label>First Name</label>
                     <input
@@ -91,14 +153,5 @@ function Form() {
                       BOOK DEMO
                     </button>
                   </div>
-                </form>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-    </>
-  );
+                </form> */
 }
-
-export default Form;
